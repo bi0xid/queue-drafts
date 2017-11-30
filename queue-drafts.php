79488ddba3a9631d90d226c408509c6f18610d93
Rafa Poveda <raven@raven.es>
2017-11-30 12:19:18 +0100

    initializing plugin

---
+++ queue-drafts.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Queue-drafts
+ * Plugin URI: https://wordpress.org/plugins/queue-drafts
+ * Description: Allow user to queue the drafts in the system.
+ * Version: 0.1
+ * Author: bi0xid
+ * Author URI: https://raven.es
+ * License: GPL3
+ */
